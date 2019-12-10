bf24e4513073cfc60b752495bfb906f255a40b96
Michelle Siregar <michellesiregar@gmail.com>
2019-12-10 01:42:18 +0000

    add loop for game

--- game.rb
+++ game.rb
@@ -1,4 +1,6 @@
 class Game
+  attr_accessor :current_player
+
   def initialize
     @player_1 = Player.new("Player 1")
     @player_2 = Player.new("Player 2")
@@ -15,7 +17,7 @@ class Game
 
   def create_new_round
     q = Question.new
-    puts "#{@current_player}: What does #{q.num1} plus #{q.num2} equal?"
+    puts "#{@current_player.name}: What does #{q.num1} plus #{q.num2} equal?"
     player_answer = gets.chomp.to_i
 
     if player_answer == q.correct_answer
@@ -25,10 +27,21 @@ class Game
       puts "You are wrong"
       @current_player.reduce_number_of_lives
     end
+
+    self.compare_lives
   end
 
   def compare_lives
-    puts "#{@player_1.number_of_lives} and #{player_2.number_of_lives}"
+    puts "P1: #{@player_1.number_of_lives}/3 vs P2: #{@player_2.number_of_lives}/3"
   end
 
+  def continue_game?
+    while @player_1.number_of_lives > 0 && @player_2.number_of_lives > 0 do
+      self.create_new_round
+      self.change_current_player
+    end
+    
+    puts "GAME OVER"
+  end
+  
 end
--- main.rb
+++ main.rb
@@ -3,5 +3,4 @@ require "./question"
 require "./player"
 
 game = Game.new
-game.create_new_round
-game.compare_lives
+game.continue_game?
